5b8eddc0f723643eb233ba5ca4f40c4c527f8908
daisuke <dsaito85a@gmail.com>
2018-10-25 12:01:57 +0900

    first commit

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+ ini_set('display_errors', 1);
+ define('DSN' , 'mysql:host=localhost;dbname=dotinstall_todo_app');
+ define('DB_USERNAME' ,'dotinstalluser');
+ define('DB_PASSWORD' , 'testtest');
+
