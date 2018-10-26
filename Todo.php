c6ddd6bf05ccd2d5be472821d6b2bf7d465082e5
daisuke <dsaito85a@gmail.com>
2018-10-26 09:20:30 +0900

    add token for CSRF

--- Todo.php
+++ Todo.php
@@ -1,11 +1,17 @@
 <?php
 
+// CSRF対策
+// Token発行してSessionに格納
+// フォームからもTokenを発行、送信
+// Check
+
 namespace MyApp;
 
 class Todo {
 	private $_db;
 
 	public function __construct(){
+		$this->_createToken();
 		try{
 			$this->_db = new \PDO(DSN, DB_USERNAME, DB_PASSWORD);
 			$this->_db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
@@ -13,6 +19,13 @@ class Todo {
 			echo $e->getMessage();
 			echo "PDOException";
 			exit;
+		}	
+	}
+
+		
+	private function _createToken(){
+		if(!isset($_SESSION['token'])){
+			$_SESSION['token'] = bin2hex(openssl_random_pseudo_bytes(15));
 		}
 	}
 
@@ -22,8 +35,9 @@ class Todo {
 	}
 	
 	public function post(){
+		$this->_validateToken();
 		if(!isset($_POST['mode'])){
-		 throw new \Exception('mode not set!');
+		  throw new \Exception('mode not set!');
 		}
 		
 		switch( $_POST['mode']) {
@@ -36,7 +50,16 @@ class Todo {
 		}
 
 	}
-	
+	private function _validateToken(){
+		if(
+			!isset($_SESSION['token']) ||	
+			!isset($_POST['token']) ||
+			$_SESSION['token'] !== $_POST['token']
+		){
+			throw new \Exception('invalid token!');
+		}
+	}
+
 	private function _update(){
 			
 		if(!isset($_POST['id'])){
